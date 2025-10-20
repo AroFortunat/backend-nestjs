e8d291e50d4962c2f3b02bf40f80b12e6ec1b813
AroFortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-10-20 14:42:46 +0300

    feat: implement constructor in PostService to inject PrismaService

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private readonly prismaService: PrismaService) {}
+}
