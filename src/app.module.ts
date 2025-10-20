fed8ef827f0133a4eb506b55e1d43e4944b97e8f
AroFortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-10-20 16:08:29 +0300

    feat(post) : add protected route by jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PositionModule } from './position/position.module';
-import { TeamModule } from './team/team.module';
 import { AuthModule } from './auth/auth.module';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PositionModule, TeamModule, AuthModule, PostModule],
+  imports: [AuthModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { PostService } from './post.service';
-
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
@@ -8,8 +19,23 @@ export class PostController {
   showAllPost() {
     return this.postService.getAllPost();
   }
-  @Get()
-  showPostById(id: string) {
+  @Get('/:id')
+  showPostById(@Param('id') id: string) {
     return this.postService.getPostById(id);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  createPost(@Body() data: CreatePostDto) {
+    return this.postService.createPost(data);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Put('/:id')
+  updatePost(@Param('id') id: string, @Body() data: UpdatePostDto) {
+    return this.postService.updatePost(id, data);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Delete('/:id')
+  deletePost(@Param('id') id: string) {
+    return this.postService.deletePost(id);
   }
 }
